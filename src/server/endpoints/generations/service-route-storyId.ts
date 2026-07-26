71e92a8fa6a3e1c9b700c84b2b2763facd735b9a
NQQT <devilz.angelz@gmail.com>
2026-07-26 16:58:00 +1000

    Updated Configuration

---
+++ src/server/endpoints/generations/service-route-storyId.ts
@@ -0,0 +1,15 @@
+import { asServiceHandler } from '@underload/service';
+import { generationCreateNewStory } from './generation-create-new-story';
+import { generationDeleteStory } from './generation-delete-story';
+import { generationGetStoryData } from './generation-get-story-data';
+import { generationUpdateChapter } from './generation-update-chapter';
+
+export default {
+    route: '/v1/storyboard/generations/:storyId',
+    handler: asServiceHandler({
+        POST: generationCreateNewStory,
+        DELETE: generationDeleteStory,
+        GET: generationGetStoryData,
+        PATCH: generationUpdateChapter
+    })
+};
